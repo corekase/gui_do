587b4b1d3d6b6bc3cdd6b69339e53e643baf92b3
John Pickett <pickett.bill@gmail.com>
2025-05-23 00:43:47 -0230

    comments

--- gui_do.py
+++ gui_do.py
@@ -1,4 +1,5 @@
+# gui_do is a package composed of the gui directory.  Use that package for client programs
 if __name__ == '__main__':
+    # if this file is launched then start the demo
     from demo.demo import Demo
-    # Launch program
     Demo().run()
