acfa4efdc77d9eface8e1573a6fc89fdd24c9467
John Pickett <pickett.bill@gmail.com>
2025-05-22 22:24:20 -0230

    move scaling fix into package

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,3 +1,4 @@
+import os
 # import utility module into this packages namespace
 from . import utility
 from .utility import cut, cut_tile, image_alpha, file_resource, padding, render_text, centre
@@ -10,3 +11,11 @@ from .label import Label
 from .button import Button
 from .scrollbar import Scrollbar
 from .pushbuttongroup import PushButtonGroup
+
+if os.name == 'nt':
+    # fixes graphical scaling issues with Windows
+    # to reproduce issue: run on a 4k display with the screen resolution being 1920x1080
+    # with FULLSCREEN and SCALED flags and Windows OS system scaling set to 150%
+    # without setting DPIAware below
+    import ctypes
+    ctypes.windll.user32.SetProcessDPIAware()
--- gui_test.py
+++ gui_test.py
@@ -1,17 +1,8 @@
-import os
 import pygame
 from pygame import Rect, FULLSCREEN, SCALED, QUIT
 from pygame.locals import MOUSEMOTION, KEYDOWN, K_ESCAPE
 from gui import GuiManager, Frame, Label, Button, PushButtonGroup, Scrollbar
 from gui import file_resource, image_alpha, cut, centre, render_text, set_font
-
-if os.name == 'nt':
-    # fixes graphical scaling issues with Windows
-    # to reproduce issue: run on a 4k display with the screen resolution being 1920x1080
-    # with FULLSCREEN and SCALED flags and Windows OS system scaling set to 150%
-    # without setting DPIAware below
-    import ctypes
-    ctypes.windll.user32.SetProcessDPIAware()
 
 class Main:
     def __init__(self):
