a764541f587dc46bcde679a0e617e88f1e520364
John Pickett <pickett.bill@gmail.com>
2025-05-23 01:19:50 -0230

    improve package initialization

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,18 +1,17 @@
+# gui is a package
 import os
-# import utility module into this packages namespace
-from . import utility
+# import utility functions into this package's namespace
 from .utility import cut, cut_tile, image_alpha, file_resource, padding, render_text, centre
 from .utility import load_font, set_font, gprint
-# import gui manager and controls into this packages namespace
+# import GuiManager and controls into this package's namespace
 from .guimanager import GuiManager
 from .frame import Frame
 from .label import Label
 from .button import Button
 from .scrollbar import Scrollbar
 from .pushbuttongroup import PushButtonGroup
-
+# fix font graphical scaling issues with Windows
 if os.name == 'nt':
-    # fixes graphical scaling issues with Windows
     # to reproduce issue: run on a 4k display with the screen resolution being 1920x1080
     # with FULLSCREEN and SCALED flags and Windows OS system scaling set to 150%
     # without setting DPIAware below
