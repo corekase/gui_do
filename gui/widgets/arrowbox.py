02db605cc4c4207d1dd2e712906af94f547b53d8
John Pickett <pickett.bill@gmail.com>
2025-05-27 06:07:55 -0230

    add arrowbox stub

---
+++ gui/widgets/arrowbox.py
@@ -0,0 +1,4 @@
+# a box that displays a triangle graphic pointing at an angle.  Up, Down, Left, and Right presets
+# are built in.  Or pass a degree between 0 and 360 during initialization
+#
+# this widget will be used for scrollbar buttons controlling the bar
