d8c665fafce99aa76bf6e995323977953f37d4b2
John Pickett <pickett.bill@gmail.com>
2025-05-27 05:24:33 -0230

    add dialogbox stub

---
+++ gui/forms/dialogbox.py
@@ -0,0 +1,3 @@
+# given a rect and a list of button options and a message:
+# display the message and a grid of the button list items, wait for a selection
+# and return a signal for which selection
