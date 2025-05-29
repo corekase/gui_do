89c3b235200daa79785eed5e9dd3316f87e9951d
John Pickett <pickett.bill@gmail.com>
2025-05-29 15:56:47 -0230

    comment

--- gui/guimanager.py
+++ gui/guimanager.py
@@ -201,7 +201,7 @@ class GuiManager:
             return self.event({'mousebuttondown': event.button})
         if event.type == MOUSEMOTION:
             return self.event({'mousemotion': event.rel})
-        # no event at all matched anything
+        # no event matched anything
         return self.event(None)
 
     def handle_widget(self, widget, event, window=None):
