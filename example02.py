4c4f0a52f05932eba034145791f9b1930cb78659
John Pickett <pickett.bill@gmail.com>
2025-05-29 17:22:17 -0230

    improve example02

--- example02.py
+++ example02.py
@@ -42,14 +42,15 @@ class Demo:
         # vertical scrollbar
         sb3 = add(Scrollbar('S2', Rect(x + w - 30, y + 40, 20, 150), False))
         sb3.set(100, 0, 30)
-        set_grid_properties((x + 10, y + 10), 100, 20, 4)
-        add(Button('exit', gridded(0, 0), 'Exit'), self.exit)
-        self.fps_label = add(Label(gridded(1, 0), 'N/A'))
-        set_grid_properties((x + 10, y + 150), 70, 20, 4)
-        self.fps_control = add(PushButtonGroup('60fps', gridded(0, 0), '60 fps', 'fps', PushButtonKind.RADIO))
-        add(PushButtonGroup('fpsupcapped', gridded(1, 0), 'Uncapped', 'fps', PushButtonKind.RADIO))
-        x, y, width, height = 0, 0, 440, 140
+        # exit button
+        add(Button('exit', Rect(x + centre(w, 100), y + 10, 100, 20), 'Exit'), self.exit)
+        # fps controls
+        set_grid_properties((x + 10, y + 135), 70, 12, 4)
+        self.fps_label = add(Label(gridded(0, 0), 'N/A'))
+        self.fps_control = add(PushButtonGroup('60fps', gridded(0, 1), '60 fps', 'fps', PushButtonKind.RADIO))
+        add(PushButtonGroup('fpsupcapped', gridded(1, 1), 'Uncapped', 'fps', PushButtonKind.RADIO))
         # position of the window
+        x, y, width, height = 0, 0, 440, 140
         window_x = centre(self.screen.get_rect().width, width)
         window_y = centre(self.screen.get_rect().height, height)
         # create the window and it adds itself to the gui_manager and makes itself the active object
@@ -58,17 +59,17 @@ class Demo:
         # set grid layout properties
         set_grid_properties((x + 10, y + 10), 140, 20, 4)
         # regular buttons
-        add(Button('Button_1', gridded(0, 0), 'Button'))
-        add(Button('Button_2', gridded(0, 1), 'Button'))
-        add(Button('Button_3', gridded(0, 2), 'Button'))
+        add(Button('Button_1', gridded(0, 0), 'Button 1'))
+        add(Button('Button_2', gridded(0, 1), 'Button 2'))
+        add(Button('Button_3', gridded(0, 2), 'Button 3'))
         # pushbutton boxes
-        self.window_push_box_widget = add(PushButtonGroup('Push 1', gridded(1, 0), 'PushButton', 'pb', PushButtonKind.BOX))
-        add(PushButtonGroup('Push 2', gridded(1, 1), 'PushButton', 'pb', PushButtonKind.BOX))
-        add(PushButtonGroup('Push 3', gridded(1, 2), 'PushButton', 'pb', PushButtonKind.BOX))
+        self.window_push_box_widget = add(PushButtonGroup('Box 1', gridded(1, 0), 'Push Box 1', 'pb', PushButtonKind.BOX))
+        add(PushButtonGroup('Box 2', gridded(1, 1), 'Push Box 2', 'pb', PushButtonKind.BOX))
+        add(PushButtonGroup('Box 3', gridded(1, 2), 'Push Box 3', 'pb', PushButtonKind.BOX))
         # pushbutton radios
-        self.window_radio_box_widget = add(PushButtonGroup('Radio 1', gridded(2, 0), 'RadioButton', 'pr', PushButtonKind.RADIO))
-        add(PushButtonGroup('Radio 2', gridded(2, 1), 'RadioButton', 'pr', PushButtonKind.RADIO))
-        add(PushButtonGroup('Radio 3', gridded(2, 2), 'RadioButton', 'pr', PushButtonKind.RADIO))
+        self.window_radio_box_widget = add(PushButtonGroup('Radio 1', gridded(2, 0), 'Push Radio 1', 'pr', PushButtonKind.RADIO))
+        add(PushButtonGroup('Radio 2', gridded(2, 1), 'Push Radio 2', 'pr', PushButtonKind.RADIO))
+        add(PushButtonGroup('Radio 3', gridded(2, 2), 'Push Radio 3', 'pr', PushButtonKind.RADIO))
         # labels
         self.window_pushbox_label = add(Label(gridded(1, 3), 'N/A'))
         self.window_radio_label = add(Label(gridded(2, 3), 'N/A'))
