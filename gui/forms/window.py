d255eadeba5ad143313d8aa7120694947c6971a9
John Pickett <pickett.bill@gmail.com>
2025-05-29 18:21:29 -0230

    add "dirty" property, and widget list of ones that are, to windows

--- gui/forms/window.py
+++ gui/forms/window.py
@@ -36,6 +36,15 @@ class Window:
         self.set_pos(pos)
         # whether a window is shown or hidden
         self.visible = True
+        # whether or not this window is dirty, starts True
+        self.dirty = True
+        # a list of dirty widgets
+        self.dirty_widgets = []
+
+    def add_dirty_widget(self, widget):
+        # widgets use this method to inform the window they are dirty
+        self.dirty = True
+        self.dirty_widgets.append(widget)
 
     def make_title_bar_graphic(self, title):
         set_font('titlebar')
