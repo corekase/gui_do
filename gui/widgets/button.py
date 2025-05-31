c58f9583dc282e45f531d96a9d1f27120e5fd457
John Pickett <pickett.bill@gmail.com>
2025-05-31 20:02:31 -0230

    rebase button on widget

--- gui/widgets/button.py
+++ gui/widgets/button.py
@@ -1,17 +1,20 @@
 from pygame.locals import MOUSEMOTION, MOUSEBUTTONDOWN, MOUSEBUTTONUP
 from ..guimanager import GuiManager
 from ..utility import convert_to_window
-from .frame import Frame, FrameState
 from ..bitmapfactory import BitmapFactory
+from .widget import Widget
+from enum import Enum
 
-class Button(Frame):
+State = Enum('State', ['Idle', 'Hover', 'Armed'])
+
+class Button(Widget):
     def __init__(self, id, rect, text):
         # initialize common widget values
         super().__init__(id, rect)
         self.gui = GuiManager()
         factory = BitmapFactory()
         self.idle, self.hover, self.armed = factory.draw_box_button_bitmaps(text, rect)
-        self.state = FrameState.IDLE
+        self.state = State.Idle
 
     def handle_event(self, event, window):
         if event.type not in (MOUSEMOTION, MOUSEBUTTONDOWN, MOUSEBUTTONUP):
@@ -20,32 +23,32 @@ class Button(Frame):
         # is the mouse position within the button rect
         collision = self.rect.collidepoint(convert_to_window(event.pos, window))
         # manage the state of the button
-        if (self.state == FrameState.IDLE) and collision:
-            self.state = FrameState.HOVER
-        if self.state == FrameState.HOVER:
+        if (self.state == State.Idle) and collision:
+            self.state = State.Hover
+        if self.state == State.Hover:
             if (event.type == MOUSEMOTION) and (not collision):
-                self.state = FrameState.IDLE
+                self.state = State.Idle
             if (event.type == MOUSEBUTTONDOWN) and collision:
                 if event.button == 1:
-                    self.state = FrameState.ARMED
-        if self.state == FrameState.ARMED:
+                    self.state = State.Armed
+        if self.state == State.Armed:
             if (event.type == MOUSEBUTTONUP) and collision:
                 if event.button == 1:
                     # button clicked
-                    self.state = FrameState.IDLE
+                    self.state = State.Idle
                     return True
             if (event.type == MOUSEMOTION) and (not collision):
-                self.state = FrameState.IDLE
+                self.state = State.Idle
         # button not clicked
         return False
 
     def leave(self):
-        self.state = FrameState.IDLE
+        self.state = State.Idle
 
     def draw(self):
-        if self.state == FrameState.IDLE:
+        if self.state == State.Idle:
             self.surface.blit(self.idle, (self.rect.x, self.rect.y))
-        elif self.state == FrameState.HOVER:
+        elif self.state == State.Hover:
             self.surface.blit(self.hover, (self.rect.x, self.rect.y))
-        elif self.state == FrameState.ARMED:
+        elif self.state == State.Armed:
             self.surface.blit(self.armed, (self.rect.x, self.rect.y))
