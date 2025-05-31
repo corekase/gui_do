4297370be7908a7548ee4d126dd9b802211aff54
John Pickett <pickett.bill@gmail.com>
2025-05-30 22:56:13 -0230

    improve toggle button

--- gui/widgets/togglebutton.py
+++ gui/widgets/togglebutton.py
@@ -1,45 +1,36 @@
 from pygame.locals import MOUSEMOTION, MOUSEBUTTONDOWN
-from .button import Button
-from .frame import FrameState
+from .widget import Widget
 from ..bitmapfactory import BitmapFactory
 from ..utility import convert_to_window
 
-class ToggleButton(Button):
-    def __init__(self, id, rect, pressed, pressed_text, raised_text=None):
-        super().__init__(id, rect, 'None')
+class ToggleButton(Widget):
+    def __init__(self, id, rect, pushed, pressed_text, raised_text=None):
+        from ..guimanager import GuiManager
+        self.gui = GuiManager()
+        super().__init__(id, rect)
+        self.pushed = pushed
         if raised_text == None:
             raised_text = pressed_text
-        if pressed:
-            self.pushed = FrameState.ARMED
-        else:
-            self.pushed = FrameState.IDLE
         factory = BitmapFactory()
-        self.text1_idle, _, self.text1_armed = factory.draw_button_bitmaps(pressed_text, rect)
-        self.text2_idle, _, self.text2_armed = factory.draw_button_bitmaps(raised_text, rect)
+        _, _, self.pressed_text_bitmap = factory.draw_button_bitmaps(pressed_text, rect)
+        self.raised_text_bitmap, _, _ = factory.draw_button_bitmaps(raised_text, rect)
 
     def handle_event(self, event, window):
-        if event.type not in (MOUSEMOTION, MOUSEBUTTONDOWN):
-            # no matching events for toggle button logic
-            return False
-        # manage the state of the toggle button
         if event.type == MOUSEBUTTONDOWN:
             if event.button == 1:
-                if self.rect.collidepoint(convert_to_window(event.pos, window)):
+                if self.rect.collidepoint(convert_to_window(self.gui.lock_area(event.pos), window)):
                     # button was clicked
-                    if self.pushed == FrameState.ARMED:
-                        self.pushed = FrameState.IDLE
-                    elif self.pushed == FrameState.IDLE:
-                        self.pushed = FrameState.ARMED
+                    self.pushed = not self.pushed
                     return True
         # button not clicked
         return False
 
     def draw(self):
-        if self.pushed == FrameState.ARMED:
-            self.surface.blit(self.text1_armed, self.rect)
+        if self.pushed:
+            self.surface.blit(self.pressed_text_bitmap, self.rect)
         else:
-            self.surface.blit(self.text2_idle, self.rect)
+            self.surface.blit(self.raised_text_bitmap, self.rect)
 
     def read(self):
         # return the state of the button as a bool
-        return self.pushed == FrameState.ARMED
+        return self.pushed
