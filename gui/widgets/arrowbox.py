3277e7bbc90674822f43f7d2a599be42517a0650
John Pickett <pickett.bill@gmail.com>
2025-05-31 13:37:12 -0230

    comments

--- gui/widgets/arrowbox.py
+++ gui/widgets/arrowbox.py
@@ -2,3 +2,6 @@
 # are built in.  Or pass a degree between 0 and 360 during initialization
 #
 # this widget will be used for scrollbar buttons controlling the bar
+#
+# arrows are always in a square area, so if the rect isn't square then the drawn arrow area will be the
+# smaller dimension, either horizontal or vertical, squared, centered in the rect
