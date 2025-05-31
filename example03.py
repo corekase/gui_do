c7b92a601f32e6b091e1cda0544c317a0a1a3ea3
John Pickett <pickett.bill@gmail.com>
2025-05-31 15:59:28 -0230

    improve example03

--- example03.py
+++ example03.py
@@ -4,7 +4,7 @@ from pygame.locals import K_ESCAPE
 from gui import set_surface, file_resource, centre, load_font, set_font, set_last_font
 from gui import set_grid_properties, gridded, set_cursor, add
 from gui import GuiManager, GKind, Window
-from gui import Button, Image, ToggleButton
+from gui import Button, Image, ToggleButton, Label
 
 class Demo:
     def __init__(self):
@@ -16,26 +16,27 @@ class Demo:
         pygame.display.set_caption('gui_do')
         # hide system mouse pointer
         pygame.mouse.set_visible(False)
+        # blit a background image
+        self.screen.blit(pygame.image.load(file_resource('images', 'watercolor-green-wallpaper-modified.jpg')).convert(), (0, 0))
 
         self.gui = GuiManager()
         set_surface(self.screen)
         load_font('normal', 'Ubuntu-Medium.ttf', 14)
         load_font('titlebar', 'Ubuntu-Medium.ttf', 10)
         load_font('gui_do', 'Ubuntu-Medium.ttf', 36)
-
+        set_font('gui_do')
+        add(Label((50, 20), 'gui_do'))
         set_font('normal')
         # exit button, uses a callback function
         add(Button('exit', Rect(10, 1050, 70, 20), 'Exit'), self.exit)
 
-        window_x, window_y, width, height = 50, 50, 180, 205
+        window_x, window_y, width, height = 50, 110, 180, 205
         Window('image_window', 'Realize', (window_x, window_y), (width, height))
 
         add(Image('image', Rect(15, 15, 145, 145), 'realize.png'))
         self.toggle = add(ToggleButton('toggle', Rect(15, 170, 145, 20), True, 'On', 'Off'))
         # set cursor image
         set_cursor((1, 1), 'cursors', 'Icons8_cursor.png')
-        # set a background image
-        self.screen.blit(pygame.image.load(file_resource('images', 'watercolor-green-wallpaper-modified.jpg')).convert(), (0, 0))
         # set running flag
         self.running = True
 
