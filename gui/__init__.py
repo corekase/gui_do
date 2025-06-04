b512891918b5258d7c442499a2218f4998c38015
John Pickett <pickett.bill@gmail.com>
2025-06-04 02:04:07 -0230

    remove unused command

--- gui/__init__.py
+++ gui/__init__.py
@@ -3,7 +3,7 @@ import os
 # import utility functions into this package's namespace
 from .command import gui_init, load_font, set_font, set_last_font, render_text, Window
 from .command import centre, set_grid_properties, gridded
-from .command import image_alpha, file_resource, copy_graphic_area, gprint
+from .command import image_alpha, file_resource, copy_graphic_area
 from .command import set_active_object, set_cursor, add, set_backdrop, update_pristine, restore_pristine
 from .command import Scrollbar
 # import GuiManager and widgets into this package's namespace
