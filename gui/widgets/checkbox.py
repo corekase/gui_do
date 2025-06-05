e46268fcdb0dae5ef5b2e6076efcc0e1b16149a6
John Pickett <pickett.bill@gmail.com>
2025-06-05 15:10:30 -0230

    comments

--- gui/widgets/checkbox.py
+++ gui/widgets/checkbox.py
@@ -1,3 +1,5 @@
 # checkbox widget
 # a widget with a checked and unchecked bitmap that can be either on the left or right
 # of a text label of the box
+#
+# checkbox is a togglebutton with different bitmaps
