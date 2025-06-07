02692cd337059b53078c515bb3d87c2e0cddd1ea
John Pickett <pickett.bill@gmail.com>
2025-06-07 12:36:00 -0230

    stub example07

---
+++ example07.py
@@ -0,0 +1,91 @@
+# this is a template file to use as a starting point for your own applications,
+# copy this template to your client folder and rename it to your application name
+#
+import pygame
+from pygame import Rect, FULLSCREEN, SCALED
+from pygame.locals import K_ESCAPE
+from gui import gui_init, set_backdrop, load_font, set_font
+from gui import add, set_cursor
+from gui import GKind, Label, Button
+
+class Template:
+    def __init__(self):
+        # initialize pygame
+        pygame.init()
+        # create main window surface
+        self.screen = pygame.display.set_mode((1920, 1080), FULLSCREEN | SCALED)
+        # set window caption
+        pygame.display.set_caption('gui_do')
+        # hide system mouse pointer
+        pygame.mouse.set_visible(False)
+        # create a gui manager
+        self.gui = gui_init(self.screen)
+        # blit a background image to the screen surface
+        set_backdrop('backdrop.jpg')
+        # load fonts
+        load_font('titlebar', 'Ubuntu-Medium.ttf', 10)
+        load_font('normal', 'Ubuntu-Medium.ttf', 14)
+        load_font('gui_do', 'Ubuntu-Medium.ttf', 36)
+        # main label
+        set_font('gui_do')
+        self.gui_do_label = add(Label((50, 50),'gui_do', automatic_pristine=True))
+        set_font('normal')
+        # exit button, uses a callback function
+        add(Button('exit', Rect(10, 1050, 70, 20), 'Exit'), self.exit)
+        # set cursor image
+        set_cursor((1, 1), 'cursor.png')
+        # set running flag
+        self.running = True
+
+    def run(self):
+        # fps to maintain, if 0 then unlimited
+        fps = 60
+        # a pygame clock to control the fps
+        clock = pygame.time.Clock()
+        # whether to draw the boxes
+        while self.running:
+            # handle events
+            self.handle_events()
+            # draw gui
+            self.gui.draw_gui()
+            # buffer to the screen
+            pygame.display.flip()
+            # tick to desired frame-rate
+            clock.tick(fps)
+            # undraw gui
+            self.gui.undraw_gui()
+        # release resources
+        pygame.quit()
+
+    def handle_events(self):
+        # update internal gui timers
+        self.gui.timers.update()
+        # handle the pygame event queue
+        for raw_event in pygame.event.get():
+            # process event queue
+            event = self.gui.handle_event(raw_event)
+            if event.type == GKind.Pass:
+                # no operation
+                continue
+            if event.type == GKind.Quit:
+                # handle window close widget or alt-f4 keypress
+                self.running = False
+                return
+            if event.type == GKind.Widget:
+                pass
+                # if event.widget_id == 'widget_id':
+                #     pass
+                # elif event.widget_id == 'next_id':
+                #     pass
+            elif event.type == GKind.KeyDown:
+                # handle key presses
+                if event.key == K_ESCAPE:
+                    # handle escape key
+                    self.running = False
+
+    # callbacks
+    def exit(self):
+        self.running = False
+
+if __name__ == '__main__':
+    Template().run()
