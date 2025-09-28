eab271175122040a26b798a5286f5e22369df580
John Pickett <pickett.bill@gmail.com>
2025-09-28 15:39:14 -0230

    stub editor.py

---
+++ editor.py
@@ -0,0 +1,4 @@
+# this will be a graphical editor for windows and widgets
+# definitions which are stored in a simple text format and one
+# of the editor functions is to output the definitions as
+# source-code form to be used for your programs
