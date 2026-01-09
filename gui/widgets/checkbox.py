a1c667d23167a1be254fc97a849521a0148ecc3b
John Pickett <pickett.bill@gmail.com>
2026-01-09 00:17:32 -0330

    remove unneeded stub

--- gui/widgets/checkbox.py
+++
@@ -1,6 +0,0 @@
-# checkbox widget
-# a widget with a checked and unchecked bitmap that can be either on the left or right
-# of a text label of the box
-#
-# checkbox is a togglebutton with different bitmaps
-# size of the checkbox rect is the size of the bitmap graphic and the text bitmap
