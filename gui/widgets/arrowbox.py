48156af1a66954e74b4cafec6d91095d694bde97
John Pickett <pickett.bill@gmail.com>
2026-04-10 08:52:59 -0230

    make magic number an enumeration

--- gui/widgets/arrowbox.py
+++ gui/widgets/arrowbox.py
@@ -1,11 +1,11 @@
 from ..bitmapfactory import BitmapFactory
-from ..constants import GType
+from ..constants import GType, BtnStyl
 from .button import Button
 
 class ArrowBox(Button):
     def __init__(self, id, rect, direction, callback=None):
         # initialize common widget values, skipping the button factory with the True
-        super().__init__(id, rect, 1, None, callback, True)
+        super().__init__(id, rect, BtnStyl.Boxed, None, callback, True)
         self.GType = GType.Arrowbox
         factory = BitmapFactory()
         self.idle, self.hover, self.armed = factory.draw_arrow_state_bitmaps(rect, direction)
