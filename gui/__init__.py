eeedd98a513fb16f7cc159e96567d0788f0af37e
John Pickett <pickett.bill@gmail.com>
2026-04-13 02:20:50 -0230

    cleanup imports

--- gui/__init__.py
+++ gui/__init__.py
@@ -8,16 +8,8 @@ from .command import set_cursor, add, set_pristine, restore_pristine
 from .command import set_buffered
 # import GuiManager and widgets into this package's namespace
 from .guimanager import GuiManager
-from .forms.window import Window
 from .constants import colours, GKind, GType, HorV, SArrows, BStyle
-from .widgets.frame import Frame, FrState
-from .widgets.label import Label
-from .widgets.button import Button
-from .widgets.buttongroup import ButtonGroup
-from .widgets.image import Image
-from .widgets.toggle import Toggle
-from .widgets.canvas import Canvas, CKind
-from .widgets.scrollbar import Scrollbar
+from .widgets.canvas import CKind
 from .scheduler import Scheduler, TKind
 # fix font graphical scaling issues with Windows
 if os.name == 'nt':
